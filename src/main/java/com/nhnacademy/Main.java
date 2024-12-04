4d468831cb5ad2405461b08e20df98dc213e44dd
gimzaezin <gimzaezin@gmail.com>
2024-12-04 15:56:45 +0900

    init

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -2,7 +2,7 @@ package com.nhnacademy;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("AAAA");
         System.out.println("Hello world!");
         System.out.println("vvvv");
         System.out.println("VVVVVV");
