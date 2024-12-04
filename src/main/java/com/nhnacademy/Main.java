29b48c1a98109efaa0c0d73ce41d26217d9b49e5
gimzaezin <gimzaezin@gmail.com>
2024-12-04 15:37:19 +0900

    init

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("VVVVVV");
         System.out.println("aaaaa");
         System.out.println("VVVVVAAAAAa");
+        System.out.println("QQQQQQ");
     }
 }
